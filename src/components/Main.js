75c4277c0fa6f2733edd10997bf713fab74b9b64
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-06-04 11:18:58 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import PokemonCard from './PokemonCard';
 
 const Main = (totalCard) => {
   const [pokemons, setPokemons] = useState([]);
@@ -9,8 +10,27 @@ const Main = (totalCard) => {
 
   useEffect(() => {
     fetchPokemons(`https://pokeapi.co/api/v2/pokemon?limit=${totalCard}`);
-
+    // eslint-disable-next-line
   }, []);
+
+  const fetchPokemons = async (url) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      const pokemonResults = data.results;
+
+      const fetchedPokemons = await Promise.all(
+        pokemonResults.map(async (pokemon) => {
+          const res = await fetch(pokemon.url);
+          const pokemonData = await res.json();
+          return pokemonData;
+        })
+      );
+
+
+
+
 
   return (
     <>
